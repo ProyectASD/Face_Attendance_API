64f14bb16fc21033cf4abc940abd9a6ad4bd6ec6
Steven <sirpepega777@gmail.com>
2024-11-02 21:26:05 -0500

    validación de codigo en actualizar curso

--- src/controllers/curso_controller.js
+++ src/controllers/curso_controller.js
@@ -53,8 +53,17 @@ import mongoose from "mongoose"
         try {
             if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({msg: "Lo sentimos pero el id no es válido"})
             if(Object.values(req.body).includes("")) return res.status(400).json({msg: "Lo sentimos todos los campos deben de estar llenos"})
-    
-            const cursoEncontrado = await Cursos.findByIdAndUpdate(id, req.body)
+            let cursoEncontrado
+            try {
+                cursoEncontrado = await Cursos.findByIdAndUpdate(id, req.body,{
+                    runValidators: true
+                })
+            } catch (error) {
+                //Validacion de campo unico
+                if(error.code === 11000) return res.status(404).json({msg: "Código ya existe. Elija uno diferente"}) 
+            }    
+
+            if(req.body?.codigo === cursoEncontrado.codigo) return res.status(404).json({msg: "Lo sentimos, "})
             if(!cursoEncontrado) return res.status(404).json({msg: "Lo sentimos pero el curso no se encuentra registrado"})
             await cursoEncontrado.save()
             res.status(200).json({msg: "Curso actualizado con éxito"})
